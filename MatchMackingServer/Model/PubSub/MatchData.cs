bc19c70c5d8edb9b83acbfe7a7fbdd365d8afe41
HongStudy <fnvl7855@naver.com>
2024-05-16 01:02:18 +0900

    매칭 데이터 변경

--- MatchMackingServer/Model/PubSub/MatchData.cs
+++ MatchMackingServer/Model/PubSub/MatchData.cs
@@ -2,12 +2,6 @@ public class MatchingData
 {
     public PublishType Type { get; set; }
     public Int64 MatchID { get; set; }
-    public MatchingUserData? MatchingUserData { get; set; }
-    public MatchingServerInfo? MatchingServerInfo { get; set; }
-}
-
-public class MatchingUserData
-{
     public string FirstUserID { get; set; } = null!;
     public string SecondUserID { get; set; } = null!;
 }
